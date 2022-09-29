94fd522bc50f6c4cad83434b022d5415789e34b4
Marymuriga <marymuriga@gmail.com>
2022-09-29 09:36:17 +0300

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,12 +7,12 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
+  # def create
+  #   bird = Bird.create(params.permit(:name, :species))
+  #   render json: bird, status: :created
+  # end
 
-  # GET /birds/:id
+  #GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
@@ -20,6 +20,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private 
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
